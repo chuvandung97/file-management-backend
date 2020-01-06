dd2f91d69fc5d018bd10dcb210a18b1083d3bcca
dungcv97 <dung@gmail.com>
2020-01-06 16:38:45 +0700

    fix api get folderfile

--- routes/folderfile.js
+++ routes/folderfile.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models')
+var Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
-//danh sách folder và file
+//danh sách folder và file  
 router.get('/lists', async function(req, res, next) {
     try {
         let active = req.query.active 
         let search = req.query.search ? req.query.search : false
+        let name = req.query.name
+        let size = req.query.size
+        let time = req.query.time
         let storage = await models.storage.findOne({
             attributes: ['id'],
             where: { name: req.query.storage_id }
@@ -15,11 +20,44 @@ router.get('/lists', async function(req, res, next) {
             return res.status(404).json({code: 404, message: "Kho không tồn tại"})
         }
         let storageId = storage.dataValues.id
+
+        var fileCondition = {
+            storage_id: storageId,
+            active: active,
+        }
+        if(name) {
+            fileCondition.name = { [Op.substring]: name }
+        }
+        if(size) {
+            let sizeArr = size.split(':')
+            switch (sizeArr[0]) {
+                case 'less':
+                    fileCondition.size = { [Op.lt]: Number(sizeArr[1]) }
+                    break;
+                case 'more':
+                    fileCondition.size = { [Op.gt]: Number(sizeArr[1]) }
+                    break
+                default:
+                    fileCondition.size = { [Op.between]: [Number(sizeArr[1]), Number(sizeArr[3])] }
+                    break;
+            } 
+        }
+        if(time) {
+            let timeArr = time.split(':')
+            switch (timeArr[0]) {
+                case 'after':
+                    fileCondition.updatedAt = { [Op.gt]: timeArr[1] }
+                    break;
+                case 'before':
+                    fileCondition.updatedAt = { [Op.between]: [timeArr[3], timeArr[1]] }
+                    break
+                default:
+                    fileCondition.updatedAt = null
+                    break;
+            }
+        }
         let fileList = await models.file.findAll({
-            where: {
-                storage_id: storageId,
-                active: active
-            },
+            where: fileCondition,
             include: [{model: models.folder}, {model: models.User}, {model: models.filetypedetail}, {model: models.filehistory, include: [models.User, models.filetypedetail]}],
             order: [
                 [models.filehistory, 'updatedAt','desc'],
@@ -28,35 +66,37 @@ router.get('/lists', async function(req, res, next) {
         })
 
         if(search) {
-            var folderCondition = {
-                storage_id: storageId,
-                active: active
-            }
+            var folderCondition = fileCondition
         } else {
             var folderCondition = {
                 storage_id: storageId,
                 parent_id: req.query.parent_id ? req.query.parent_id : null,
                 active: active
             }
+
         }
         
-        let folderList = await models.folder.findAll({
-            where: folderCondition,
-            order: [
-                ['createdAt', 'ASC']
-            ], 
-            include: [{ model: models.storage, include: [
-                models.User
-            ] }, { model: models.folder, as: 'children', include: {
-                model: models.folder, as: 'children', include: {
+        if(folderCondition.size) {
+            var folderList = []
+        } else {
+            var folderList = await models.folder.findAll({
+                where: folderCondition,
+                order: [
+                    ['createdAt', 'ASC']
+                ], 
+                include: [{ model: models.storage, include: [
+                    models.User
+                ] }, { model: models.folder, as: 'children', include: {
                     model: models.folder, as: 'children', include: {
                         model: models.folder, as: 'children', include: {
-                            model: models.folder, as: 'children'
+                            model: models.folder, as: 'children', include: {
+                                model: models.folder, as: 'children'
+                            }
                         }
                     }
-                }
-            } }, {model: models.User}],
-        })
+                } }, {model: models.User}],
+            })
+        }
         if(active == 1 && !search) {
             var folderFileList = folderList.concat(fileList.filter(el => el.folders == 0))
         } else {
